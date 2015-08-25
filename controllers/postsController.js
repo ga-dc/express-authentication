1f5f589ba1caa07dc2ac31dc0665652748c31d86
Jesse Shawl <jesse@jshawl.com>
2015-08-24 21:22:00 -0400

    create and delete working

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,5 +5,22 @@ module.exports = {
     Post.findAll().then(function(posts){
       res.render("posts/index",{posts: posts})
     }) 
+  },
+  create: function(req, res){
+    Post.create({
+      title: req.body.title,
+      body: req.body.body
+    }).then(function(post){
+      res.redirect("/posts") 
+    })	  
+  },
+  destroy: function(req, res){
+    Post.destroy({
+      where: {
+        id: req.params.id	     
+      }
+    }).then(function(){
+      res.redirect("/posts")
+    })
   }
 }
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require("express")
 var app = express()
+var methodOverride = require('method-override')
+var bodyParser = require('body-parser')
+app.use(methodOverride('_method'))
+app.use(bodyParser({urlencode: true}))
 app.set("view engine","hbs")
 var postsController = require("./controllers/postsController")
 
 app.get("/posts", postsController.index)
-/*
+app.delete("/posts/:id", postsController.destroy)
 app.post("/posts", postsController.create)
+/*
 app.get("/posts/new", postsController.new)
 app.get("/posts/:id", postsController.show)
-app.delete("/posts/:id", postsController.destroy)
 */
 
-app.listen(3000, function(){
+app.listen(3001, function(){
   console.log("listening on port 3000")
 })
